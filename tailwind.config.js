17982aa0bcd32277afa426e313c77774df957dbc
HellblazerXXL <royceapera@yahoo.com>
2025-05-27 14:10:44 -0500

    adds changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
  /** @type {import('tailwindcss').Config} */
  export default {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "360px",
+      md: "768px",
+      lg: "1024px"
+    },
     extend: {},
   },
   plugins: [],
